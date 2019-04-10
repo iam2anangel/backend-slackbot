90bb792886983d395d6f42f435cfc523f3593050
Jennifer Browning <browningjen@yahoo.com>
2019-04-10 12:53:57 -0400

    bot is connected to slack

--- slackbot.py
+++ slackbot.py
@@ -13,51 +13,139 @@ BOT_NAME = 'my-bot-name'
 BOT_CHAN = '#bot-test'
 
 
-def config_logger():
-    """Setup logging configuration"""
+import os
+import time
+import re
+from slackclient import SlackClient
+from dotenv import load_dotenv
+
+# load_dotenv()
+
+# token = os.getenv("SLACK_BOT_TOKEN")
+# type(token)
+# print("Token is {}".format(token))
+
+r = requests.get("https://anapioficeandfire.com/api/characters/583")
+# instantiate Slack client
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+# starterbot's user ID in Slack: value is assigned after the bot starts up
+starterbot_id = None
+
+# constants
+RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
+EXAMPLE_COMMAND = "do"
+MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+
+def parse_bot_commands(slack_events):
+    """
+        Parses a list of events coming from the Slack RTM API to find bot commands.
+        If a bot command is found, this function returns a tuple of command and channel.
+        If its not found, then this function returns None, None.
+    """
+    for event in slack_events:
+        if event["type"] == "message" and not "subtype" in event:
+            user_id, message = parse_direct_mention(event["text"])
+            if user_id == starterbot_id:
+                return message, event["channel"]
+    return None, None
+
+def parse_direct_mention(message_text):
+    """
+        Finds a direct mention (a mention that is at the beginning) in message text
+        and returns the user ID which was mentioned. If there is no direct mention, returns None
+    """
+    matches = re.search(MENTION_REGEX, message_text)
+    # the first group contains the username, the second group contains the remaining message
+    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+
+def handle_command(command, channel):
+    """
+        Executes bot command if the command is known
+    """
+    # Default response is help text for the user
+    default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
+
+    # Finds and executes the given command, filling in response
+    response = None
+    # This is where you start to implement more commands!
+    if command.startswith(EXAMPLE_COMMAND):
+        response = "Sure...write some more code then I can do that!"
+
+    # Sends the response back to the channel
+    slack_client.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=response or default_response
+    )
 
 
-def command_loop(bot):
-    """Process incoming bot commands"""
-    pass
+if __name__ == "__main__":
+    if slack_client.rtm_connect(with_team_state=False):
+        print("Starter Bot connected and running!")
+        # Read bot's user ID by calling Web API method `auth.test`
+        starterbot_id = slack_client.api_call("auth.test")["user_id"]
+        while True:
+            command, channel = parse_bot_commands(slack_client.rtm_read())
+            if command:
+                handle_command(command, channel)
+            time.sleep(RTM_READ_DELAY)
+    else:
+        print("Connection failed. Exception traceback printed above.")
 
 
-def signal_handler(sig_num, frame):
-    pass
 
 
-class SlackBot:
-
-    def __init__(self, bot_user_token, bot_id=None):
-        """Create a client instance"""
-        pass
-
-    def __repr__(self):
-        pass
-
-    def __str__(self):
-        pass
-
-    def __enter__(self):
-        """Implement this method to make this a context manager"""
-        pass
-
-    def __exit__(self, type, value, traceback):
-        """Implement this method to make this a context manager"""
-        pass
-
-    def post_message(self, msg, chan=BOT_CHAN):
-        """Sends a message to a Slack Channel"""
-        pass
-
-    def handle_command(self, raw_cmd, channel):
-        """Parses a raw command string from the bot"""
-        pass
 
 
-def main():
-    pass
 
 
-if __name__ == '__main__':
-    main()
+# def config_logger():
+#     """Setup logging configuration"""
+
+
+# def command_loop(bot):
+#     """Process incoming bot commands"""
+#     pass
+
+
+# def signal_handler(sig_num, frame):
+#     pass
+
+
+# class SlackBot:
+
+#     def __init__(self, bot_user_token, bot_id=None):
+#         """Create a client instance"""
+#         pass
+
+#     def __repr__(self):
+#         pass
+
+#     def __str__(self):
+#         pass
+
+#     def __enter__(self):
+#         """Implement this method to make this a context manager"""
+#         pass
+
+#     def __exit__(self, type, value, traceback):
+#         """Implement this method to make this a context manager"""
+#         pass
+
+#     def post_message(self, msg, chan=BOT_CHAN):
+#         """Sends a message to a Slack Channel"""
+#         pass
+
+#     def handle_command(self, raw_cmd, channel):
+#         """Parses a raw command string from the bot"""
+#         pass
+
+
+# def main():
+#     pass
+
+
+# if __name__ == '__main__':
+#     main()
+
+
